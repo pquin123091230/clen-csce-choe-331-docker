25d628f5d811a3163f037e88d0d79caee4c73618
bugses man <quinnp@tamu.edu>
2025-01-23 10:12:14 -0800

    part 1 csv reader

--- public/index.php
+++ public/index.php
@@ -43,7 +43,72 @@
 <!-- PHP testing area ................................ --> 
 <?php
 
-   echo "<h1> Choe's Experimental CSCE 331 Docker Web Site </h1>\n";
+    function proc_csv2($fileName, $columns) {
+        $realColumns = [];
+        if ($columns == "ALL") {
+            $realColumns = [-1];
+        }
+        else{
+            $columns = preg_split("/:/", $columns);
+            foreach ($columns as $column) {
+                array_push($realColumns, intval($column)-1);
+            }
+        }
+        $handle = fopen($fileName,"r") or die("Cannot open data.dat");
+
+   echo "<table  border=\"1\">\n";
+
+   $row = 0;
+   while ($data = fgets($handle)) {
+        
+        echo "<tr>\n";
+        $inQuote = '';
+        $data_cols = [];
+
+        $last = 0;
+        for ($i = 0; $i < strlen($data); $i++) {
+            if ($data[$i] == '"' || $data[$i] == "'") {
+                if ($inQuote == '') {
+                    $inQuote = $data[$i];
+                } else if ($inQuote == $data[$i]) {
+                    $inQuote = '';
+                }
+            }
+            else if ($data[$i] == ',' || $data[$i] == '\t') {
+                //In vscode use of tab is replaced by spaces
+                if ($inQuote == '') {
+                    //Only split if not inside quotes
+                    $tmpStr = substr($data, $last, $i - $last);
+                    array_push($data_cols, $tmpStr);
+                    $last = $i+1;
+                    //+1 to skip the splitter character
+                }
+            }
+        }
+        array_push($data_cols, substr($data, $last));
+        //Push last value
+
+        //$data_cols = preg_split('/,/',$data);
+        for ($k=0; $k<count($data_cols); ++$k) {
+            if (in_array($k, $realColumns) || in_array(-1, $realColumns)) {
+                if ($row == 0) {
+                    echo "  <td> <h3>".$data_cols[$k]."</h3></td>\n";
+                }
+                else {
+                    echo "  <td> ".$data_cols[$k]." </td>\n";
+                }
+            }
+        }
+        echo "</tr>\n";
+        $row+=1;
+   }
+
+   fclose($handle);
+
+   echo "</table>\n<p/>";
+   
+    }
+   echo "<h1> Patrick Quinn 331 Project 1 </h1>\n";
 
    echo "<font color=\"green\"> Haha update and redeploy </font><p/>\n";
    echo " Testing PHP <br>\n";
@@ -54,17 +119,54 @@
    # FILE access 
    # $h = fopen("false.dat","r");
 
-   $handle = fopen("data.dat","r") or die("Cannot open data.dat");
+   proc_csv2("data.dat", "1:3");
+
+   proc_csv2("data.dat", "ALL");
+
+   /*$handle = fopen("data.dat","r") or die("Cannot open data.dat");
 
    echo "<table  border=\"1\">\n";
 
+   $row = 0;
    while ($data = fgets($handle)) {
+        
         echo "<tr>\n";
-        $data_cols = preg_split('/,/',$data);
+        $inQuote = '';
+        $data_cols = [];
+
+        $last = 0;
+        for ($i = 0; $i < strlen($data); $i++) {
+            if ($data[$i] == '"' || $data[$i] == "'") {
+                if ($inQuote == '') {
+                    $inQuote = $data[$i];
+                } else if ($inQuote == $data[$i]) {
+                    $inQuote = '';
+                }
+            }
+            else if ($data[$i] == ',' || $data[$i] == '\t') {
+                if ($inQuote == '') {
+                    //Only split if not inside quotes
+                    $tmpStr = substr($data, $last, $i - $last);
+                    array_push($data_cols, $tmpStr);
+                    $last = $i+1;
+                    //+1 to skip the splitter character
+                }
+            }
+        }
+        array_push($data_cols, substr($data, $last));
+        //Push last value
+
+        //$data_cols = preg_split('/,/',$data);
         for ($k=0; $k<count($data_cols); ++$k) {
-            echo "  <td> ".$data_cols[$k]." </td>\n";
+            if ($row == 0) {
+                echo "  <td> <h3>".$data_cols[$k]."</h3></td>\n";
+            }
+            else {
+                echo "  <td> ".$data_cols[$k]." </td>\n";
+            }
         }
         echo "</tr>\n";
+        $row+=1;
    }
 
    fclose($handle);
@@ -79,7 +181,7 @@
 
    echo "rand : ".$data_cols[rand(0,1)]."\n";
 
-   echo "<p/>";
+   echo "<p/>";*/
 
 ?>
 
